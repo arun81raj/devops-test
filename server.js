ad0a9ff43930d70a6757efba644affcf64bc0e2d
arun <raj.amrit143@gmail.com>
2026-01-04 18:52:17 +0530

    First

---
+++ server.js
@@ -0,0 +1,184 @@
+const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
+
+const app = express();
+app.use(express.json());
+
+// --------------------
+// In-memory storage
+// --------------------
+let users = [];
+let nextId = 1;
+
+// --------------------
+// Swagger configuration
+// --------------------
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "In-Memory CRUD API",
+      version: "1.0.0",
+      description: "CRUD REST API with in-memory data and Swagger",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./server.js"], // location of Swagger comments
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// --------------------
+// Routes
+// --------------------
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - name
+ *         - email
+ *       properties:
+ *         id:
+ *           type: integer
+ *           example: 1
+ *         name:
+ *           type: string
+ *           example: John Doe
+ *         email:
+ *           type: string
+ *           example: john@example.com
+ */
+
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Create a user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       201:
+ *         description: User created
+ */
+app.post("/users", (req, res) => {
+  const user = {
+    id: nextId++,
+    name: req.body.name,
+    email: req.body.email,
+  };
+  users.push(user);
+  res.status(201).json(user);
+});
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Get all users
+ *     responses:
+ *       200:
+ *         description: List of users
+ */
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+/**
+ * @swagger
+ * /users/{id}:
+ *   get:
+ *     summary: Get user by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: User found
+ *       404:
+ *         description: User not found
+ */
+app.get("/users/:id", (req, res) => {
+  const user = users.find(u => u.id == req.params.id);
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  res.json(user);
+});
+
+/**
+ * @swagger
+ * /users/{id}:
+ *   put:
+ *     summary: Update a user
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: User updated
+ *       404:
+ *         description: User not found
+ */
+app.put("/users/:id", (req, res) => {
+  const user = users.find(u => u.id == req.params.id);
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  user.name = req.body.name;
+  user.email = req.body.email;
+  res.json(user);
+});
+
+/**
+ * @swagger
+ * /users/{id}:
+ *   delete:
+ *     summary: Delete a user
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: User deleted
+ */
+app.delete("/users/:id", (req, res) => {
+  users = users.filter(u => u.id != req.params.id);
+  res.json({ message: "User deleted" });
+});
+
+// --------------------
+// Start server
+// --------------------
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+  console.log("Swagger docs at http://localhost:3000/api-docs");
+});
